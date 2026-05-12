292535133a745cc5525a65aa498cfcf40048af33
Jeanpierre-Arteaga <jeanpierre.arteaga@unmsm.edu.pe>
2026-05-12 17:18:23 -0500

    Eliminar generar-hash.js del repositorio (queda solo en local)

--- generar-hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-async function generarHash() {
-    const hash = await bcrypt.hash('123456', 10);
-    console.log('Hash para "123456":');
-    console.log(hash);
-}
-
-generarHash();
